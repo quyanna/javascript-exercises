aee5430b9e625bf32ad04f2272f468539afde25b
quyanna <quyannacampbell@gmail.com>
2022-11-11 03:29:27 -0700

    completed exercises 02-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arry, ...removeArry) {
 
+    for (i = 0; i < removeArry.length; i++){
+
+        let x = arry.indexOf(removeArry[i]);
+        while(x >= 0){
+            arry.splice(x, 1);
+            x = arry.indexOf(removeArry[i]);
+        }
+    }
+
+    return arry;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
 
+    if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR"
+    }
+    else if (n1 < 0 || n2 < 0) {
+        return "ERROR";
+    }
+
+    if (n1 > n2) { // swap if larger value first 
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+
+    let sum = 0;
+    for (i = n1; i <= n2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
